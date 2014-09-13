8002c0db549a035d5a532b0087cba73ba9eb65d4
angelali92 <angela@li>
2014-09-13 16:04:45 -0700

    preparing for push to heroku

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,7 @@ app.controller('iucCtrl', function($scope) {
 
 app.controller('fetchCtrl', ['$scope', '$http', '$templateCache', function($scope, $http, $templateCache) {
 		$scope.method = 'GET';
-		$scope.url = 'http-hello.html';
+		$scope.url = 'schools.html';
 
 		$scope.fetch = function() {
 			$scope.code = null;
